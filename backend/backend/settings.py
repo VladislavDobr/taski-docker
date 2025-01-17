c5286b83af49f6dce2100162da7c84c592b3aa97
VladislavDobr <mr_dobryak19@mail.ru>
2025-01-17 23:09:37 +0300

    Add Test6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = "django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^"
+SECRET_KEY = os.environ.get("SECRET", "your_default_secret_key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["123.123.123.123", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = str(os.environ.get("HOSTS")).split(",")[
+    "notespractic.ru", "localhost", "127.0.0.1"
+]
 
 
 # Application definition
